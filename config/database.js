80e1455c3b14818b2ca6e39def0cfd47ec53ab43
AxelRock11121 <102250503+AxelRock11121@users.noreply.github.com>
2024-08-09 10:15:34 -0600

    cambio de conexion
    subo cambio de coneccion a azure

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('VEHICULOSPOWER', 'axel', 'Axel_2024', {
-    host: 'localhost',
+// const sequelize = new Sequelize('VEHICULOSPOWER', 'axel', 'Axel_2024', {
+//     host: 'localhost',
+//     dialect: 'mssql',
+//     port: 1433, // Asegúrate de usar el puerto correcto
+// });
+
+const sequelize = new Sequelize('VEHICULOSPOWER', 'axel', 'P@ssw0rd123!', {
+    host: 'axelpower.database.windows.net',
     dialect: 'mssql',
     port: 1433, // Asegúrate de usar el puerto correcto
 });
